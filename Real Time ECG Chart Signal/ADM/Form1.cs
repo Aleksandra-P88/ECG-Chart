b3e9ef36f3c021507642314f589a01aa82773ca3
Aleksandra-P88 <aleksandra.podwojcik@gmail.com>
2020-01-24 19:29:44 +0100

    To do files

---
+++ Real Time ECG Chart Signal/ADM/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+using System.Diagnostics;
+using rtChart;
+using MathNet.Numerics;
+using MathNet.Numerics.IntegralTransforms;
+using System.Numerics;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace ADM
+{
+   
+
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+           
+        }
+
+      
+        Ecg ecg = new Ecg();
+
+         
+
+  
+        public static List<int> readlines(Signal signal)
+        {
+
+
+           return  signal.read_line();
+           
+        }
+
+
+
+        public  double updates()
+        {
+           
+            
+            return ecg.update();
+        }
+
+
+      
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+ 
+            Func<double> del1 = updates;
+
+            kayChart ecg1 = new kayChart(chart1, 1000);
+
+           
+            // kayChart imp = new kayChart(chart1, 1000);
+            //kayChart time = new kayChart(chart3, 1000);
+
+            Task.Factory.StartNew(() =>
+            {
+
+                ecg1.updateChart(updates, 40);
+                // imp.updateChart(upadateImp, 40);
+                // time.updateChart(upadateTime, 40);
+
+
+            }
+            );
+
+
+
+
+            int[] nr_linii = readlines(ecg).ToArray();
+
+
+            foreach (int item in nr_linii)
+            {
+
+                listBox1.Items.Add(item);
+
+                
+
+
+            }
+
+           
+
+
+           
+            chart1.ChartAreas[0].AxisX.Maximum = 1000;
+            chart1.ChartAreas[0].AxisX.Minimum = 0;
+            chart1.ChartAreas[0].AxisY.Maximum = readlines(ecg).Max();
+            chart1.ChartAreas[0].AxisY.Minimum = readlines(ecg).Min();
+            chart1.ChartAreas[0].AxisX.MajorGrid.LineWidth = 0;
+            chart1.ChartAreas[0].AxisY.MajorGrid.LineWidth = 0;
+
+
+            
+
+
+        }
+
+
+
+        
+
+
+
+
+    }
+}
---
+++ Real Time ECG Chart Signal/ADM/Signal.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Diagnostics;
+using rtChart;
+using System.IO;
+using MathNet.Numerics;
+using MathNet.Numerics.IntegralTransforms;
+using System.Numerics;
+using System.Windows.Forms.DataVisualization.Charting;
+
+
+namespace ADM
+{
+
+    public class Signal
+    {
+
+
+        protected string line;
+        protected string[] lines;
+        protected string lines_0;
+        string lines_P="";
+        public int i = 0;
+        public List<int> lista = new List<int>();
+        protected string sciezka = "C:/Users/user/Desktop/dane2.txt";
+      
+        public virtual List<int> read_line()
+        {
+
+            StreamReader sr = new StreamReader((sciezka));
+
+
+
+            while ((line = sr.ReadLine()) != null)
+            {
+
+               // lines = line.Split(';');
+               // lines_P = lines[0].Remove(0, 1);
+                lista.Add(int.Parse(lines_P));
+
+            }
+
+            return lista;
+        }
+
+
+
+
+
+        public virtual double update()
+        {
+
+
+
+            if (i == 999)
+            {
+                i = 0;
+
+            }
+
+            double sample_ecg = read_line()[i];
+
+            i++;
+
+            return sample_ecg;
+
+
+        }
+
+
+       
+
+    }
+
+    public class Ecg : Signal
+    {
+
+
+        public override List<int> read_line()
+        {
+
+            StreamReader sr = new StreamReader((sciezka));
+
+
+
+            while ((line = sr.ReadLine()) != null)
+            {
+
+                lines = line.Split(';');
+                lines_0 = lines[1].Remove(0, 1);
+                lista.Add(int.Parse(lines_0));
+
+            }
+            return lista;
+        }
+
+
+
+        public override double update()
+        {
+
+            if (i == 999)
+            {
+                i = 0;
+
+            }
+
+
+            double sample_ecg = read_line()[i];
+
+            i++;
+
+            return sample_ecg;
+
+
+        }
+    }
+
+
+
+}
